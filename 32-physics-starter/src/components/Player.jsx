eed64c50d1f71ae2361a8a8e9ceaf1cfa11e127a
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-11-30 21:43:01 +0000

    implement jumping & fix falling and prevent double jump

--- 32-physics-starter/src/components/Player.jsx
+++ 32-physics-starter/src/components/Player.jsx
@@ -5,12 +5,17 @@ import { Controls } from "../App";
 import { useRef } from "react";
 import { Vector3 } from "three";
 const MOVEMENT_SPEED = 5;
+const JUMP_FORCE = 8;
 
 export const Player = () => {
   const rb = useRef();
   const [, get] = useKeyboardControls();
   const vel = new Vector3();
+  const inTheAir = useRef(false);
+
   useFrame(() => {
+    const curVel = rb.current?.linvel();
+
     vel.x = 0;
     vel.y = 0;
     vel.z = 0;
@@ -26,7 +31,11 @@ export const Player = () => {
     if (get()[Controls.right]) {
       vel.x += MOVEMENT_SPEED;
     }
-    if (get()[Controls.jump]) {
+    if (get()[Controls.jump] && !inTheAir.current) {
+      vel.y += JUMP_FORCE;
+      inTheAir.current = true;
+    } else {
+      vel.y = curVel.y;
     }
     rb.current.setLinvel(vel, true);
   });
