2374fe1efafe086b1fe86eca2a8e4814c9b1012f
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-11-30 21:34:52 +0000

    make the cube move

--- 32-physics-starter/src/components/Player.jsx
+++ 32-physics-starter/src/components/Player.jsx
@@ -1,8 +1,38 @@
+import { useKeyboardControls } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 import { RigidBody } from "@react-three/rapier";
+import { Controls } from "../App";
+import { useRef } from "react";
+import { Vector3 } from "three";
+const MOVEMENT_SPEED = 5;
 
 export const Player = () => {
+  const rb = useRef();
+  const [, get] = useKeyboardControls();
+  const vel = new Vector3();
+  useFrame(() => {
+    vel.x = 0;
+    vel.y = 0;
+    vel.z = 0;
+    if (get()[Controls.forward]) {
+      vel.z -= MOVEMENT_SPEED;
+    }
+    if (get()[Controls.back]) {
+      vel.z += MOVEMENT_SPEED;
+    }
+    if (get()[Controls.left]) {
+      vel.x -= MOVEMENT_SPEED;
+    }
+    if (get()[Controls.right]) {
+      vel.x += MOVEMENT_SPEED;
+    }
+    if (get()[Controls.jump]) {
+    }
+    rb.current.setLinvel(vel, true);
+  });
+
   return (
-    <RigidBody>
+    <RigidBody ref={rb} lockRotations>
       <mesh position-y={0.5} castShadow>
         <boxGeometry args={[1, 1, 1]} />
         <meshStandardMaterial color="hotpink" />
