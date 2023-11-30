62f43c0e2d00dcb639c7425d2eaab0dd03ce7630
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-11-30 21:24:15 +0000

    define map and controls in the App

--- 32-physics-starter/src/App.jsx
+++ 32-physics-starter/src/App.jsx
@@ -1,18 +1,38 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { useMemo } from "react";
+import { KeyboardControls } from "@react-three/drei";
 
 import { Physics } from "@react-three/rapier";
 
+export const Controls = {
+  forward: "forward",
+  back: "back",
+  left: "left",
+  right: "right",
+  jumpt: "forward",
+};
+
 function App() {
+  const map = useMemo(
+    () => [
+      { name: Controls.forward, keys: ["ArrowUp", "KeyW"] },
+      { name: Controls.back, keys: ["ArrowDown", "KeyS"] },
+      { name: Controls.left, keys: ["ArrowLeft", "KeyA"] },
+      { name: Controls.right, keys: ["ArrowRight", "KeyD"] },
+      { name: Controls.jump, keys: ["Space"] },
+    ],
+    []
+  );
   return (
-    <>
+    <KeyboardControls map={map}>
       <Canvas camera={{ position: [0, 6, 6], fov: 60 }} shadows>
         <color attach="background" args={["#171720"]} />
         <Physics>
           <Experience />
         </Physics>
       </Canvas>
-    </>
+    </KeyboardControls>
   );
 }
 
