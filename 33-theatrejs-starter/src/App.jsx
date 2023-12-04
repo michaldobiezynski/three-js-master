00bfb3106aa8c24b002100104458da7b23a4d22e
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-12-04 21:44:06 +0000

    working version of theatre js

--- 33-theatrejs-starter/src/App.jsx
+++ 33-theatrejs-starter/src/App.jsx
@@ -1,28 +1,72 @@
 import { SoftShadows } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { useState } from "react";
-import { UI } from "./UI";
-import { Experience } from "./components/Experience";
 import { getProject } from "@theatre/core";
-import { SheetProvider } from "@theatre/r3f";
+import { PerspectiveCamera, SheetProvider, editable as e } from "@theatre/r3f";
 import extension from "@theatre/r3f/dist/extension";
 import studio from "@theatre/studio";
-import { PerspectiveCamera } from "@theatre/r3f";
-import { useRef } from "react";
-import { editable as e } from "@theatre/r3f";
+import { useEffect, useRef, useState } from "react";
+import { UI } from "./UI";
+import projectState from "./assets/MedievalTown.theatre-project-state.json";
+import { Experience } from "./components/Experience";
 
-studio.initialize();
-studio.extend(extension);
+export const isProd = import.meta.env.MODE === "production";
+
+if (!isProd) {
+  studio.initialize();
+  studio.extend(extension);
+}
+const project = getProject(
+  "MedievalTown",
+  isProd
+    ? {
+        state: projectState,
+      }
+    : undefined
+);
+const mainSheet = project.sheet("Main");
+
+const transitions = {
+  Home: [0, 5],
+  Castle: [6, 12 + 22 / 30],
+  Windmill: [13 + 2 / 30, 17 + 23 / 30],
+};
 
 function App() {
-  const [currentScreen, setCurrentScreen] = useState("Intro");
-  const [targetScreen, setTargetScreen] = useState("Home");
-
-  const project = getProject("MedievalTown");
-  const mainSheet = project.sheet("Main");
-
   const cameraTargetRef = useRef();
 
+  // editable(focusTargetRef.current, "focusTarget");
+
+  const [currentScreen, setCurrentScreen] = useState("Intro");
+  const [targetScreen, setTargetScreen] = useState("Home");
+  const isSetup = useRef(false);
+
+  useEffect(() => {
+    project.ready.then(() => {
+      if (currentScreen === targetScreen) {
+        return;
+      }
+      if (isSetup.current && currentScreen === "Intro") {
+        // Strict mode in development will trigger the useEffect twice, so we need to check if it's already setup
+        return;
+      }
+      isSetup.current = true;
+      const reverse = targetScreen === "Home" && currentScreen !== "Intro";
+      const transition = transitions[reverse ? currentScreen : targetScreen];
+      if (!transition) {
+        return;
+      }
+
+      mainSheet.sequence
+        .play({
+          range: transition,
+          direction: reverse ? "reverse" : "normal",
+          rate: reverse ? 2 : 1,
+        })
+        .then(() => {
+          setCurrentScreen(targetScreen);
+        });
+    });
+  }, [targetScreen]);
   return (
     <>
       <UI
@@ -32,26 +76,31 @@ function App() {
       />
       <Canvas
         camera={{ position: [5, 5, 10], fov: 30, near: 1 }}
-        shadows
         gl={{
           preserveDrawingBuffer: true,
-        }}>
+        }}
+        shadows
+      >
         <SoftShadows />
         <SheetProvider sheet={mainSheet}>
+          <e.fog theatreKey="Fog" attach="fog" args={["#cc7b32", 3, 5]} />
           <PerspectiveCamera
             position={[5, 5, 10]}
             fov={30}
             near={1}
             makeDefault
             theatreKey="Camera"
+            lookAt={cameraTargetRef}
           />
           <e.mesh
             theatreKey="Camera Target"
             visible="editor"
-            ref={cameraTargetRef}>
+            ref={cameraTargetRef}
+          >
             <octahedronBufferGeometry args={[0.1, 0]} />
             <meshPhongMaterial color="yellow" />
           </e.mesh>
+
           <Experience />
         </SheetProvider>
       </Canvas>
