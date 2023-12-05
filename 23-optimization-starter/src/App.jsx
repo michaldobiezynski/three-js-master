04ad123a14eec6e366733e0684fde28ec885af8f
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-12-05 21:16:18 +0000

    hide the effect when performance decline and half the number of boxes

--- 23-optimization-starter/src/App.jsx
+++ 23-optimization-starter/src/App.jsx
@@ -1,28 +1,37 @@
 import { Environment, PerformanceMonitor } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { useState } from "react";
 import { Effects } from "./components/Effects";
 import { Experience } from "./components/Experience";
-import React from "react";
 
 function App() {
-  const [nbBoxes, setNbBoxes] = React.useState(1000);
-
+  const [effect, setEffect] = useState(true);
+  const [nbBoxes, setNbBoxes] = useState(20000);
   return (
     <>
       <Canvas camera={{ position: [0, 2, 10], fov: 42 }}>
+        <PerformanceMonitor
+          onChange={(api) => {
+            console.log("Performance Monitor (FPS)", api.fps);
+            console.log("Performance Monitor (Factor)", api.factor);
+          }}
+          onIncline={() => {
+            setEffect(true);
+            console.log("Performance Monitor (Inclined)");
+          }}
+          onDecline={() => {
+            setEffect(false);
+            setNbBoxes(nbBoxes / 2);
+            console.log("Performance Monitor (Declined)");
+          }}
+        />
         <color attach="background" args={["#ffffff"]} />
         <fog attach="fog" args={["#ffffff", 10, 50]} />
-        <PerformanceMonitor
-          onChange={(api) => console.log(api)}
-          onIncline={({ incline }) => console.log(`Incline: ${incline}`)}
-          onDecline={({ decline }) => console.log(`Decline: ${decline}`)}
-          nbBoxes={nbBoxes}
-        />
         <group position-y={-2}>
-          <Experience />
+          <Experience nbBoxes={nbBoxes} />
         </group>
         <Environment preset="sunset" />
-        <Effects />
+        {effect && <Effects />}
       </Canvas>
     </>
   );
